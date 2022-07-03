e95c6a0310b7f0a2b76abbd51b495a5fe4c04ef3
Robert Glossop <robgssp@gmail.com>
2022-07-02 21:38:34 -0500

    Better error checking in link.hs

--- link.hs
+++ link.hs
@@ -1,8 +1,11 @@
+#!/usr/bin/env runhaskell
+
 import System.Posix.Files
 import System.Directory
 import System.Environment
 import Control.Monad
 import Control.Exception
+import Text.Printf
 
 files = [("zshrc", ".zshrc")
         ,("zprofile", ".zprofile")
@@ -16,5 +19,17 @@ main =
      forM_ files $
        \(from,to) ->
          handle (print::IOError -> IO ())
-                (createSymbolicLink (cwd ++ "/" ++ from)
-                                    (home ++ "/" ++ to))
+                (ensureLink (cwd ++ "/" ++ from)
+                            (home ++ "/" ++ to))
+
+ensureLink from to = do
+  exists <- doesPathExist to
+  if exists then do
+    symlink <- pathIsSymbolicLink to
+    unless symlink $
+      fail (printf "Cannot link %s: path exists" to)
+    target <- getSymbolicLinkTarget to
+    unless (target == from) $
+      fail (printf "Cannot link %s: link already points to %s" to target)
+    else do
+    createFileLink from to
