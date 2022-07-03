b131c8dc8843ed47bd863bc8ceb2f633fa26a728
Rob Glossop <robgssp@gmail.com>
2022-07-02 23:12:54 -0500

    Make .ssh/config a little more useful

--- link.hs
+++ link.hs
@@ -4,6 +4,7 @@ import System.Posix.Files
 import System.Directory
 import System.Environment
 import System.FilePath
+import System.Posix.Files
 import Control.Monad
 import Control.Exception
 import Text.Printf
@@ -16,14 +17,19 @@ files = [("zshrc", ".zshrc")
         ,("default.el", ".emacs.d/straight/versions/default.el")
         ,("sshconfig", ".ssh/config")]
 
+press = handle (print::IOError -> IO ())
+
 main =
   do cwd <- getCurrentDirectory
      home <- getEnv "HOME"
      forM_ files $
        \(from,to) ->
-         handle (print::IOError -> IO ())
-                (ensureLink (cwd ++ "/" ++ from)
-                            (home ++ "/" ++ to))
+         press (ensureLink
+                (cwd </> from)
+                (home </> to))
+     -- Create .ssh/sockets with mode 700
+     setFileCreationMask (unionFileModes groupModes otherModes)
+     ensureDirsExist (home </> ".ssh/sockets")
 
 ensureLink from to = do
   exists <- doesPathExist to
