892af70fcb6fc3c75848ad2197f68754e6031642
Rob Glossop <robgssp@gmail.com>
2024-12-07 20:40:47 -0800

    basic-init.el

---
+++ basic-init.el
@@ -0,0 +1,21 @@
+;;; Packages
+(setq straight-use-package-by-default t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package standard-theme
+  :config
+  (load-theme 'standard-dark t))
